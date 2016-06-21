508e14c1814741a0d6e09de06862e629e9ef97b5
Dylan Lott <lott.dylan@gmail.com>
2016-06-21 01:45:52 -0600

    font awesome working, layout is nearly done, auth service is coded out

--- src/client/app/user/AuthFactory.js
+++ src/client/app/user/AuthFactory.js
@@ -10,28 +10,44 @@
   function authFactory($http, $q, exception, logger) {
     var service = {
       login: loginUser,
-      logout: getMessageCount
+      logout: logoutUser, 
+      checkLoggedIn: checkLoggedIn, 
+      getUser: getUser
     };
 
     return service;
 
     function loginUser() {
-      return $http.post('/user/login')
+      return $http.post('api/user/login')
+      .then(success)
+      .catch(fail);
     }
-    function getMessageCount() { return $q.when(72); }
 
-    function getPeople() {
+    function logoutUser(){
+      return $http.get('/api/logout')
+      .then(success)
+      .catch(fail);
+    }
+
+    function checkLoggedIn() {
+      return $http.get('/api/user/authed')
+      .then(success)
+      .catch(fail);
+    }
+
+    function getUser() {
       return $http.get('/api/user')
         .then(success)
         .catch(fail);
+    }
 
-      function success(response) {
-        return response.data;
-      }
+    function success(res){
+      return res.data; 
+    }
 
-      function fail(e) {
-        return exception.catcher('XHR Failed for getPeople')(e);
-      }
+    function fail(e){
+      console.log(this); 
+      return exception.catcher('XHR Failed - AuthUser')(e); 
     }
   }
 })();
